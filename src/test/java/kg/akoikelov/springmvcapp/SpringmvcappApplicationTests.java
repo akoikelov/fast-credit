77ac9ecef97ad3eeb1be518402f287dfa0b63d70
Aikol Koikelov <kolyakoikelov@MacBook-Pro-Zeynep.local>
2021-01-31 03:43:08 +0600

    Init spring app project

---
+++ src/test/java/kg/akoikelov/springmvcapp/SpringmvcappApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.akoikelov.springmvcapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmvcappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
