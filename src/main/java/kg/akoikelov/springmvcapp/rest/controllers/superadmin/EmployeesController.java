152f270ec3e1811111a728e0193afd5bd1bee7e8
Aikol Koikelov <aikolkoikelov@Aikols-MacBook-Pro.local>
2021-03-18 00:58:46 +0600

    FAS 70 add get employee list and by id

--- src/main/java/kg/akoikelov/springmvcapp/rest/controllers/superadmin/EmployeesController.java
+++ src/main/java/kg/akoikelov/springmvcapp/rest/controllers/superadmin/EmployeesController.java
@@ -7,9 +7,7 @@ import kg.akoikelov.springmvcapp.services.EmployeeService;
 import kg.akoikelov.springmvcapp.utils.ControllerHelper;
 import kg.akoikelov.springmvcapp.utils.PaginationData;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
 @Api(tags = "superadmin", value = "employees")
 @RestController
@@ -29,11 +27,7 @@ public class EmployeesController {
     }
     @GetMapping ("/{id}")
     public Employee getById(@PathVariable int id){
-        Employee employee = employeeService.getEmployee(id);
-        if (employee ==null){
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-        }
-        return employee;
+        return employeeService.getEmployee(id);
     }
 
 }
