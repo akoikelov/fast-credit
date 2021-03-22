c414a6d762c5c6958e81a3cfa5769466262d028c
Aikol Koikelov <aikolkoikelov@Aikols-MacBook-Pro.local>
2021-03-22 23:18:02 +0600

    FAS 70 rest employee create update

--- src/main/java/kg/akoikelov/springmvcapp/utils/PaginationData.java
+++ src/main/java/kg/akoikelov/springmvcapp/utils/PaginationData.java
@@ -1,10 +1,14 @@
 package kg.akoikelov.springmvcapp.utils;
 
+import com.fasterxml.jackson.annotation.JsonView;
+
 import java.util.List;
 
 public class PaginationData<T> {
 
+    @JsonView(ViewsRest.forList.class)
     private final List<T> data;
+    @JsonView(ViewsRest.forList.class)
     private final int allCount;
 
     public PaginationData(List<T> data, int allCount) {
---
+++ src/main/java/kg/akoikelov/springmvcapp/utils/ViewsRest.java
@@ -0,0 +1,6 @@
+package kg.akoikelov.springmvcapp.utils;
+
+public final class ViewsRest {
+    public interface forList {
+    }
+}
